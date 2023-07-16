87e97d3f66ce1086757024a039349661b1d589a1
Guilherme Cardozo Seemann <104910701+Guilherme020205@users.noreply.github.com>
2023-07-16 19:08:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,54 +2,84 @@ var perguntas = [
   /*Pergunta*/[],
   /*Alternativa A*/[],
   /*Alternativa B*/[],
-  /*Alternativa C*/[]
+  /*Alternativa C*/[],
+  /*Alternativa Correta*/[]
 ];
 
+var pontosPositivos = 0
+var pontosNegativos = 0
 
-for (let i = 0; i < 2; i++) {
-  //Número da pergunta
-  var nPergunta = i
-  //questão é a certa ou não
-  var questãoCerta
-  var pergunta = prompt(`Cadastre a ${nPergunta + 1}° questão `);
-  perguntas[0].push(pergunta);
-
-  var alternativaA = prompt(`Cadastre a primeira alternativa `);
-  perguntas[1].push(alternativaA);
-
-  questãoCerta = prompt(`Está e a alternativa correta? Sim(s) ou Não(n)`);
-
-  if (questãoCerta == "s") {
-    alternativaA = true;
-  } else {
-    alternativaA = false;
+do {
+  if(perguntas[0].length > 0){
+    var opcao = Number(prompt(`Menu
+  (1)Registrar pergunta.
+  (2)Responder pergunta.
+  (3)Sair`));
+  }else{
+    var opcao = Number(prompt(`Menu
+  (1)Registrar pergunta.
+  (2)(opção indisponível, cadastre as perguntas!)
+  (3)Sair`));
   }
+  
+  switch (opcao) {
+    case 1:
 
-  var alternativaB = prompt(`Cadastre a segunda alternativa `);
-  perguntas[2].push(alternativaB);
-  questãoCerta = prompt(`Está e a alternativa correta? Sim(s) ou Não(n)`);
+      //cadastrar perguntas
+      for (let i = 0; i < 1; i++) {
+        //Número da pergunta
+        // var nPergunta = i
+        //questão é a certa ou não
+        var questãoCerta
 
-  if (questãoCerta == "s") {
-    alternativaA = true;
-  } else {
-    alternativaA = false;
+        //cadastrar pergunta
+        var pergunta = prompt(`Cadastre a ${(i + 1)}° questão `);
+        perguntas[0].push(pergunta);
+
+        //cadastrar alternativa A
+        var alternativaA = prompt(`Cadastre a primeira alternativa `);
+        perguntas[1].push(alternativaA);
+
+        //cadastrar alternativa B
+        var alternativaB = prompt(`Cadastre a segunda alternativa `);
+        perguntas[2].push(alternativaB);
+
+        //cadastrar alternativa C
+        var alternativaC = prompt(`Cadastre a terceira alternativa `);
+        perguntas[3].push(alternativaC);
+
+        //pergunta qual das alternativa está correta
+
+        questãoCerta = prompt(`Qual é a alternativa correta? (A), (B) OU (C)`);
+        perguntas[4].push(questãoCerta);
+      }
+      break;
+    case 2:
+      //respondendo as perguntas cadastradas
+      if (perguntas[0].length > 0){
+        for (let i = 0; i < 1; i++) {
+
+          let resposta = prompt(`${perguntas[0][i]}
+  A) ${perguntas[1][i]}
+  B) ${perguntas[2][i]}
+  C) ${perguntas[3][i]}`);
+
+          if (resposta.toLowerCase() == perguntas[4][i].toLowerCase()) {
+            alert(`Parabéns a aternativa ${resposta.toUpperCase()} está correta!`)
+            pontosPositivos += 1
+          } else {
+            alert(`Infelizmente a aternativa ${resposta.toUpperCase()} está incorreta!`)
+            pontosNegativos += 1
+          }
+        }
+        //pontuação do quiz
+        alert(`Pontos Positivos ${pontosPositivos}
+Pontos Negatios ${pontosNegativos}`);
+
+      }else{
+        alert("Cadastre as perguntas antes de tentar responder!")
+      }
+      
+      break;
   }
-
-  var alternativaC = prompt(`Cadastre a terceira alternativa `);
-  perguntas[3].push(alternativaC);
-  questãoCerta = prompt(`Está e a alternativa correta? Sim(s) ou Não(n)`);
-
-  if (questãoCerta == "s") {
-    alternativaA = true;
-  } else {
-    alternativaA = false;
-  }
-
-}
-for(let i = 0; i < 2; i++){
-prompt(`${perguntas[0][0]}
-A) ${perguntas[1][0]}
-B) ${perguntas[2][0]}
-C) ${perguntas[3][0]}`);
-
-}
+} while (opcao != 3);
